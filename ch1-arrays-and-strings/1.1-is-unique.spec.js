eb06dc69561b700e5f13b3e42b617d162c96fb5b
Isaias Pomales <impomales@gmail.com>
2018-07-12 19:24:11 -0400

    define isUnique function

---
+++ ch1-arrays-and-strings/1.1-is-unique.spec.js
@@ -0,0 +1,30 @@
+const { expect } = require('chai');
+const isUnique = require('./1.1-is-unique');
+
+describe('isUnique function', () => {
+  let result;
+
+  it('returns true string with all unique characters', () => {
+    result = isUnique('abc');
+    expect(result).to.be.equal(true);
+
+    result = isUnique('TheQuickFox');
+    expect(result).to.be.equal(true);
+
+    result = isUnique('z');
+    expect(result).to.be.equal(true);
+  });
+
+  it('returns false for a string with duplicates', () => {
+    result = isUnique('aaa');
+    expect(result).to.be.equal(false);
+
+    result = isUnique('I am not unique');
+    expect(result).to.be.equal(false);
+  });
+
+  it('returns true for an empty string', () => {
+    result = isUnique('');
+    expect(result).to.be.equal(true);
+  });
+});
