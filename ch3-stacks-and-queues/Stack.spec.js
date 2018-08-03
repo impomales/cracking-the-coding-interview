c9377a85fce4b24ad5561c2c3e8e80a83130106f
Isaias Pomales <impomales@gmail.com>
2018-08-03 17:52:02 -0400

    add tests for peek and isEmpty

--- ch3-stacks-and-queues/Stack.spec.js
+++ ch3-stacks-and-queues/Stack.spec.js
@@ -28,5 +28,52 @@ describe('Stack', () => {
       removed = stack.pop();
       expect(removed).to.equal('a');
     });
+
+    it('handles popping an empty stack', () => {
+      expect(stack.pop).to.throw('stack is currently empty.');
+    });
+
+    it('handles multiple push/pops appropriately', () => {
+      stack.push(1);
+      removed = stack.pop();
+      expect(removed).to.equal(1);
+
+      stack.push(2);
+      stack.push(3);
+      removed = stack.pop();
+      expect(removed).to.equal(3);
+
+      stack.push(4);
+      stack.push(5);
+      stack.push(6);
+      removed = stack.pop();
+      expect(removed).to.equal(6);
+
+      expect(stack.toString()).to.equal('5 => 4 => 2 => end');
+    });
+  });
+
+  describe('peek method', () => {
+    it('can see the value at the top', () => {
+      expect(stack.peek()).to.equal(5);
+    });
+
+    it('does not mutate the stack', () => {
+      expect(stack.toString()).to.equal('5 => 4 => 2 => end');
+    });
+  });
+
+  describe('isEmpty method', () => {
+    it('returns false when stack is not empty', () => {
+      expect(stack.isEmpty()).to.equal(false);
+    });
+
+    it('returns true if stack is empty', () => {
+      stack.pop();
+      stack.pop();
+      stack.pop();
+
+      expect(stack.isEmpty()).to.equal(true);
+    };
   });
 });
