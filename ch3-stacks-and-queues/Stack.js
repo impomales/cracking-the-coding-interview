fca8df99dac764ceebd552211b2d6be306af6f99
Isaias Pomales <impomales@gmail.com>
2018-08-03 17:38:53 -0400

    set up Stack implementation

---
+++ ch3-stacks-and-queues/Stack.js
@@ -0,0 +1,15 @@
+function Stack() {}
+
+function Node() {}
+
+Stack.prototype.push = function() {};
+
+Stack.prototype.pop = function() {};
+
+Stack.prototype.peek = function() {};
+
+Stack.prototype.isEmpty = function() {};
+
+Stack.prototype.toString = function() {};
+
+module.exports = Stack;
