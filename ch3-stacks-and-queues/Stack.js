4e8df94f525b7cff283540a9566dac204aa38620
Isaias Pomales <impomales@gmail.com>
2018-08-03 17:57:42 -0400

    define push and toString methods

--- ch3-stacks-and-queues/Stack.js
+++ ch3-stacks-and-queues/Stack.js
@@ -1,8 +1,17 @@
-function Stack() {}
+function Stack() {
+  this.top = null;
+}
 
-function Node() {}
+function Node(value, next) {
+  this.value = value;
+  this.next = next;
+}
 
-Stack.prototype.push = function() {};
+Stack.prototype.push = function(value) {
+  const newNode = new Node(value);
+  newNode.next = this.top;
+  this.top = newNode;
+};
 
 Stack.prototype.pop = function() {};
 
@@ -10,6 +19,17 @@ Stack.prototype.peek = function() {};
 
 Stack.prototype.isEmpty = function() {};
 
-Stack.prototype.toString = function() {};
+Stack.prototype.toString = function() {
+  let curr = this.top;
+  let result = '';
+
+  while (curr) {
+    result += curr.value + ' => ';
+    curr = curr.next;
+  }
+
+  result += 'end';
+  return result;
+};
 
 module.exports = Stack;
