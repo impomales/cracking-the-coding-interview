74f70afd746aa8e8bdd3367d061945c7f8483b56
Isaias Pomales <impomales@gmail.com>
2018-08-04 17:23:25 -0400

    skeleton threeInOne data structure

---
+++ ch3-stacks-and-queues/3.1-three-in-one.js
@@ -0,0 +1,11 @@
+function ThreeInOne() {};
+
+ThreeInOne.prototype.push = function(stackNum, value) {};
+
+ThreeInOne.prototype.pop = function(stackNum) {};
+
+ThreeInOne.prototype.peek = function(stackNum) {};
+
+ThreeInOne.prototype.isEmpty = function(stackNum) {};
+
+module.exports = ThreeInOne;
