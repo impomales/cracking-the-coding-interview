d9212df0e4645371a11126b8a002376f15a0fe3f
Isaias Pomales <impomales@gmail.com>
2018-08-09 18:40:02 -0400

    define push method

--- ch3-stacks-and-queues/3.1-three-in-one.js
+++ ch3-stacks-and-queues/3.1-three-in-one.js
@@ -1,11 +1,35 @@
-function ThreeInOne() {};
+function ThreeInOne(size) {
+  this.stacks = new Array(size);
+  this.stackLength = Math.floor(this.stacks.length / 3);
+  this.tops = [-1, this.stackLength - 1, this.stackLength * 2 - 1];
+  this.lengths = [0, 0, 0];
+}
 
-ThreeInOne.prototype.push = function(stackNum, value) {};
+ThreeInOne.prototype.push = function(stackNum, value) {
+  if (this.lengths[stackNum] === this.stackLength)
+    throw new Error('stack is currently full.');
+
+  this.tops[stackNum]++;
+  this.lengths[stackNum]++;
+  this.stacks[this.tops[stackNum]] = value;
+};
 
 ThreeInOne.prototype.pop = function(stackNum) {};
 
 ThreeInOne.prototype.peek = function(stackNum) {};
 
 ThreeInOne.prototype.isEmpty = function(stackNum) {};
+
+ThreeInOne.prototype.toString = function(stackNum) {
+  let res = '',
+    begin = this.tops[stackNum],
+    end = this.tops[stackNum] - this.lengths[stackNum];
+  for (let i = begin; i > end; i--) {
+    res += this.stacks[i] + ' => ';
+  }
+
+  res += 'end';
+  return res;
+};
 
 module.exports = ThreeInOne;
