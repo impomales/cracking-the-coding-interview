b65a2b9b649b38dc2e7f023be69036dfec0d2a06
Isaias Pomales <impomales@gmail.com>
2018-08-12 16:22:21 -0400

    define tests for isRoute method

--- ch4-trees-and-graphs/4.2-route-between-nodes.spec.js
+++ ch4-trees-and-graphs/4.2-route-between-nodes.spec.js
@@ -2,11 +2,55 @@ const { expect } = require('chai');
 const isRoute = require('./4.2-route-between-nodes');
 
 describe('isRoute method', () => {
-  it('returns true if there is a path');
+  describe('directed graph', () => {
+    const graph = {
+      a: ['b'],
+      b: ['c', 'd'],
+      c: [],
+      d: []
+    };
 
-  it('returns false if there is no path');
+    it('returns true if there is a path', () => {
+      expect(isRoute(graph, 'a', 'c')).to.equal(true);
+    });
 
-  it('checks if there are paths in cyclical graphs');
+    it('returns false if there is no path', () => {
+      expect(isRoute(graph, 'd', 'a')).to.equal(false);
+    });
+  });
 
-  it('checks if there are paths in disconnected graphs');
+  describe('cyclic graphs', () => {
+    const graph = {
+      a: ['b'],
+      b: ['c'],
+      c: ['d'],
+      d: ['b']
+    };
+
+    it('returns true if there is a path', () => {
+      expect(isRoute(graph, 'a', 'd')).to.equal(true);
+    });
+
+    it('returns false if there is no path', () => {
+      expect(isRoute(graph, 'b', 'a')).to.equal(false);
+    });
+  });
+
+  describe('disconnected graphs', () => {
+    const graph = {
+      a: ['b'],
+      b: ['c', 'd'],
+      c: ['d'],
+      f: ['g'],
+      g: ['f']
+    }
+
+    it('returns true if there is a path', () => {
+      expect(isRoute(graph, 'f', 'g')).to.equal(true);
+    });
+
+    it('returns false if there is no path', () => {
+      expect(isRoute(graph, 'a', 'f')).to.equal(false);
+    });
+  });
 });
