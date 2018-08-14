64f1f7e59b6082b44ace393b9fc50bc566f7a7cf
Isaias Pomales <impomales@gmail.com>
2018-08-14 19:22:43 -0400

    fix: incorrect clause within "it" block

--- ch5-bit-manipulation/5.1-insertion.spec.js
+++ ch5-bit-manipulation/5.1-insertion.spec.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const insert = require('./5.1-insertion');
 
 describe('5.1 insert function', () => {
-  it('can insert n into m bitwise', () => {
+  it('can insert m into n bitwise', () => {
     expect(insert(0b1000000000, 10011, 2, 6)).to.equal(0b10001001100);
     expect(insert(0b110111, 1001, 1, 5)).to.equal(0b010011);
   });
