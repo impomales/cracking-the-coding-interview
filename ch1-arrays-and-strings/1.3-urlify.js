f42629c917f0641d77eaab2271ae250c64d26e7b
Isaias Pomales <impomales@gmail.com>
2018-08-23 16:57:46 -0400

    define urlify function

--- ch1-arrays-and-strings/1.3-urlify.js
+++ ch1-arrays-and-strings/1.3-urlify.js
@@ -1,3 +1,27 @@
-function urlify(string, trueLength) {}
+function urlify(strArr, trueLength) {
+  let spaceCount = 0;
+
+  for (let i = 0; i < trueLength; i++) {
+    if (strArr[i] === ' ') {
+      spaceCount++;
+    }
+  }
+
+  let index = trueLength + spaceCount * 2;
+  for (let i = trueLength - 1; i >= 0; i--) {
+
+    if (strArr[i] === ' ') {
+      strArr[index - 1] = '0';
+      strArr[index - 2] = '2';
+      strArr[index - 3] = '%';
+      index -= 3;
+    } else {
+      strArr[index - 1] = strArr[i];
+      index--;
+    }
+  }
+  console.log(strArr);
+  return strArr;
+}
 
 module.exports = urlify;
