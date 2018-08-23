58ed1cdaa64451b913e5788ae341474baaa50b35
Isaias Pomales <impomales@gmail.com>
2018-08-23 16:58:19 -0400

    change arguments to be mutable char arrays

--- ch1-arrays-and-strings/1.3-urlify.spec.js
+++ ch1-arrays-and-strings/1.3-urlify.spec.js
@@ -1,12 +1,64 @@
 const { expect } = require('chai');
 const urlify = require('./1.3-urlify');
 
+function isEqual(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+}
+
 describe('1.3 urlify function', () => {
   it('returns a string with spaces replaced with %20', () => {
-    let result = urlify('Mr John Smith      ', 13);
-    expect(result).to.equal('Mr%20John%20Smith  ');
+    let result = urlify(
+      [
+        'M',
+        'r',
+        ' ',
+        'J',
+        'o',
+        'h',
+        'n',
+        ' ',
+        'S',
+        'm',
+        'i',
+        't',
+        'h',
+        ' ',
+        ' ',
+        ' ',
+        ' '
+      ],
+      13
+    );
+    let expected = [
+      'M',
+      'r',
+      '%',
+      '2',
+      '0',
+      'J',
+      'o',
+      'h',
+      'n',
+      '%',
+      '2',
+      '0',
+      'S',
+      'm',
+      'i',
+      't',
+      'h',
+    ];
 
-    result = urlify('     ', 5);
-    expect(result).to.equal('%20%20%20%20%20');
+    expect(isEqual(result, expected)).to.equal(true);
+
+    result = urlify([' ', ' ', ' ', ' ', ' '], 1);
+    expected = ['%', '2', '0', ' ', ' ']
+    expect(isEqual(result, expected)).to.equal(true);
   });
 });
