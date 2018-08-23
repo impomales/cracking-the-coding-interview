42cbf7258ee6fe07f8885f1fb411ac3bf99e07d0
Isaias Pomales <impomales@gmail.com>
2018-08-23 15:29:08 -0400

    add tests for urlify function

--- ch1-arrays-and-strings/1.3-urlify.spec.js
+++ ch1-arrays-and-strings/1.3-urlify.spec.js
@@ -1,2 +1,12 @@
 const { expect } = require('chai');
 const urlify = require('./1.3-urlify');
+
+describe('1.3 urlify function', () => {
+  it('returns a string with spaces replaced with %20', () => {
+    let result = urlify('Mr John Smith      ', 13);
+    expect(result).to.equal('Mr%20John%20Smith  ');
+
+    result = urlify('     ', 5);
+    expect(result).to.equal('%20%20%20%20%20');
+  });
+});
